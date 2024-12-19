cf0b8df222d24457cbec91714eaac233eb32c1d2
saboyyya <juliocesarsaboya@gmail.com>
2024-12-19 20:44:44 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("linkYoutube").addEventListener("click",function(){
+    const urlYoutube = "https://www.youtube.com"
+    window.open(urlYoutube,'_blank')
+})
+
+document.getElementById("linkInstagram").addEventListener("click",function(){
+    const urlInstagram = "https://www.instagram.com/saboyyya/"
+    window.open(urlInstagram,'_blank')
+})
+
+document.getElementById("linkGithub").addEventListener("click",function(){
+    const urlGithub = "https://github.com/Saboyyya"
+    window.open(urlGithub,'_blank')
+})
